183d9f9a3ec933ab152a934356ae50c589a46d5b
Jonathan Blandon <jonathanblandonm6gmail.com>
2022-08-12 13:40:32 -0600

    "Se agrego el subprograma para mostrar el mensaje de bienvenida"

--- MainFinalProyect.java
+++ MainFinalProyect.java
@@ -1,6 +1,42 @@
-public class Main{
+package finalProyectJava;
 
-    public static void public static void main(String[] args) {
-        
+public class MainFinalProyect{
+
+    public static void main(String[] args) {
+        //matrix and vector definition
+        int [][]matris = new int[4][4]; int vectorV1[] = new int[4];
+        //Llamando al subprograma welcomeMessage
+        SubPrograms MessageDialog = new SubPrograms();
+        MessageDialog.welcomeMessage();
+
+        //option menu controled by do while and switch case
+        int menuStatus = 1;
+        do{
+            switch(){
+                case 1:
+                break;
+
+                case 2:
+                break;
+
+                case 3:
+                break;
+
+                case 4:
+                break;
+
+                case 5:
+                break;
+                case 6:
+                menuStatus = 0;
+                break;
+
+            }
+
+
+        }while(menuStatus==1);
+    
+        System.out.println("Fin del Algortimo");
+        System.exit(0);
     }
 }
---
+++ SubPrograms.java
@@ -0,0 +1,40 @@
+package finalProyectJava;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+public class SubPrograms {
+    JFrame window = new JFrame();
+    //welcome message
+    public void welcomeMessage(){
+        JOptionPane.showMessageDialog(window,
+        "Universidad Católica de Costa Rica \n" +
+        "Proyecto de Programación 1 \n" +
+        "Jonathan A. Blandón Machado \n" +
+        "Brayan Araya Rojas \n" +
+        "Agosto 2022.");
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

b77396ef1cc40e7dc59db93577b4c7fb5eb9dce0
Jonathan Blandon <jonathanblandonm6gmail.com>
2022-08-12 17:58:00 -0600

    MainFinalProyect.java

--- MatricesMenu.java
+++ MatricesMenu.java
@@ -12,7 +12,7 @@ public class MatricesMenu {//the object scanner is created
         }
         return matrix;
     }
-
+    //Method to print the elements of the matrix ordered per row and column.
     public void printMatrix(int matrix[][]){
         System.out.println("Impresion de la Matris ingresada por el usuario");
         for (int row=0; row<4; row++){
@@ -22,6 +22,21 @@ public class MatricesMenu {//the object scanner is created
             System.out.println("");
         } 
     }
+    /*calculate of the main diagonal of the matrix, those numbers are stored in a new vector called vectorV1.
+        public int mainDiagonal(int matrix[][], int vector[]){
+        int Diagonaltrace = 0;
+        for (int row=0; row<4; row++){
+            for (int column=0; column<4; column++){
+                if (row==column){
+                    System.out.println(matrix[row][column]+" ");
+                    Diagonaltrace += matrix[row][column];
+                }
+            }
+        }
+        return Diagonaltrace;
+    
+    }*/
+
 
 
 

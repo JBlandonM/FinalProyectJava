ae9576ea0dec110c644068a15d59bc0bbfc17642
Jonathan Blandon <jonathanblandonm6gmail.com>
2022-08-17 16:09:32 -0600

    MainFinalProyect.java

--- MatricesMenu.java
+++ MatricesMenu.java
@@ -22,20 +22,24 @@ public class MatricesMenu {//the object scanner is created
             System.out.println("");
         } 
     }
-    /*calculate of the main diagonal of the matrix, those numbers are stored in a new vector called vectorV1.
-        public int mainDiagonal(int matrix[][], int vector[]){
+    //calculate of the main diagonal of the matrix, those numbers are stored in a new vector called vectorV1.
+        public int mainDiagonalTrace(int matrix[][], int vector[]){
         int Diagonaltrace = 0;
+        System.out.println("- Diagonal Principal de la Matris dada -");
         for (int row=0; row<4; row++){
-            for (int column=0; column<4; column++){
+            for (int column=0; column<4; column++){//in those lines is printed the main diagonal and calculated the trace of the matrix.
                 if (row==column){
-                    System.out.println(matrix[row][column]+" ");
+                    System.out.print(matrix[row][column]+" ");
                     Diagonaltrace += matrix[row][column];
+                }else if (row!=column){
+                    System.out.print("0 ");
                 }
             }
+            System.out.println("\n");
         }
-        return Diagonaltrace;
+        return Diagonaltrace; // the value of the trace is returned to the main file, and it was called into the SOUT method.
     
-    }*/
+    }
 
 
 

ff7779b4f2699ded736e1cb4e530d3aa6cceb81a
Jonathan Blandon <jonathanblandonm6gmail.com>
2022-08-18 01:01:18 -0600

    Se implementaron dos metodos, el 4 y 6 del instrucctivo.

--- MatricesMenu.java
+++ MatricesMenu.java
@@ -1,10 +1,14 @@
 package finalProyectJava;
+import java.util.Arrays;
 import java.util.Scanner;
-public class MatricesMenu {//the object scanner is created    
-    //method matrisOne is created in the class fillmatris
+
+import javax.swing.*;
+import javax.swing.JOptionPane;
+public class MatricesMenu {  
+    //method matrisMenu class is created, and the methods, fillMatrix, printMatrix, mainDiagonalTrace, BubbleSort,
 
     public int[][] fillMatrix(int matrix[][], Scanner read){
-        System.out.println("Rellenado de la Matris, indique 16 numeros enteros por favor.");
+        System.out.println("\nRellenado de Matris, Indique 16 Números Enteros, Por Favor.");
         for (int row=0; row<4; row++){
             for (int column=0; column<4; column++){
                 matrix[row][column]= read.nextInt();
@@ -14,7 +18,7 @@ public class MatricesMenu {//the object scanner is created
     }
     //Method to print the elements of the matrix ordered per row and column.
     public void printMatrix(int matrix[][]){
-        System.out.println("Impresion de la Matris ingresada por el usuario");
+        System.out.println("\n - Impresion De La Matris -");
         for (int row=0; row<4; row++){
             for (int column=0; column<4; column++){
                 System.out.print(matrix[row][column]+ " ");
@@ -25,7 +29,7 @@ public class MatricesMenu {//the object scanner is created
     //calculate of the main diagonal of the matrix, those numbers are stored in a new vector called vectorV1.
         public int mainDiagonalTrace(int matrix[][], int vector[]){
         int Diagonaltrace = 0;
-        System.out.println("- Diagonal Principal de la Matris dada -");
+        System.out.println("\n- Diagonal Principal de la Matris -");
         for (int row=0; row<4; row++){
             for (int column=0; column<4; column++){//in those lines is printed the main diagonal and calculated the trace of the matrix.
                 if (row==column){
@@ -35,15 +39,60 @@ public class MatricesMenu {//the object scanner is created
                     System.out.print("0 ");
                 }
             }
-            System.out.println("\n");
+            System.out.println("");
         }
-        return Diagonaltrace; // the value of the trace is returned to the main file, and it was called into the SOUT method.
+        return Diagonaltrace; // the value of the trace is returned to the main file, and it was called into the SOUT method-
+    }
+    public void bubbleSort(int numberSelected, String letterSelected, int matrix[][], int vector[]){
+        //in this lines are used the number selected of the type of vector selected, Row or Column.
+        numberSelected= numberSelected-1;
+        for(int element=0; element<4;element++){
+            if(letterSelected.equals("Fila")){
+                vector[element]= matrix[numberSelected][element];
+            }else if(letterSelected.equals("Columna")){
+                vector[element]= matrix[element][numberSelected];
+            }
+        }
+        //System.out.println("\nVector copiado: "+Arrays.toString(vector));
+
+        //here is the real bubble sort.
+        for(int j=0; j<3;j++){
+            for(int i=0; i<3;i++){
+                int auxNumber=0;
+                if (vector[i]>vector[i+1]){
+                    auxNumber = vector[i];
+                    vector[i]=vector[i+1];
+                    vector[i+1]=auxNumber;
+                }
+            }
+        }
+        //System.out.println("\nVector Ordenado "+Arrays.toString(vector));
+
+        //in this lines, the sorted vector is exchanged in the matrix.
+        for(int row=0;row<4;row++){
+            for(int column=0; column<4;column++){
+            if(letterSelected.equals("Fila")){
+                matrix[numberSelected][column]=vector[column];
+            }else if(letterSelected.equals("Columna")){
+                matrix[column][numberSelected]= vector[column];
+            }
+          }
+        }
+    }
     
+    public void ScalarMultiplication(String letterSelected,int numberSelected,int matrix[][]){
+        numberSelected = numberSelected-1;
+        int ScalarNumber = Integer.parseInt(JOptionPane.showInputDialog("Por Cual Número Desea Mutiplicar La "+letterSelected+" ?" ));
+        for(int element=0; element<4;element++){
+            if(letterSelected.equals("Fila")){
+                matrix[numberSelected][element]= (matrix[numberSelected][element])*ScalarNumber ;
+            }else if(letterSelected.equals("Columna")){
+                matrix[element][numberSelected] = (matrix[element][numberSelected])*ScalarNumber ;
+            }
+        }
     }
 
-
-
-
+    
 
 
 
